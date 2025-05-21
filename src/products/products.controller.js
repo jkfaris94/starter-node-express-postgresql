142469e391f867e8c0a7d18e1199a091cc1ee8df
Johnny Faris <jkfaris@gmail.com>
2025-05-21 13:08:30 -0700

    more additions to products

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,9 +1,9 @@
 const productsService = require('./products.service');
 
-function read(req, res, next) {
-  res.json({ data: { product_title: "some product title" } });
+function read(req, res) {
+  const { product: data } = res.locals;
+  res.json({ data });
 }
-
 function list(req, res, next) {
   productsService
     .list()
@@ -11,7 +11,20 @@ function list(req, res, next) {
     .catch(next);
 }
 
+function productExists(req, res, next) {
+  productsService
+    .read(req.params.productId)
+    .then((product) => {
+      if (product) {
+        res.locals.product = product;
+        return next();
+      }
+      next({ status: 404, message: `Product cannot be found.` });
+    })
+    .catch(next);
+}
+
 module.exports = {
-  read: [read],
+  read: [productExists, read],
   list: [list],
 };
