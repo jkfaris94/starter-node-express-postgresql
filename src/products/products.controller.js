2e5178437ee8eccd767dfa3410e2fe4c4ca399be
Johnny Faris <jkfaris@gmail.com>
2025-05-24 21:44:58 -0700

    updated code from then catch to async await

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -4,24 +4,19 @@ function read(req, res) {
   const { product: data } = res.locals;
   res.json({ data });
 }
-function list(req, res, next) {
-  productsService
-    .list()
-    .then((data) => res.json({ data }))
-    .catch(next);
+
+async function list(req, res, next) {
+  const data = await productsService.list();
+  res.json({ data });
 }
 
-function productExists(req, res, next) {
-  productsService
-    .read(req.params.productId)
-    .then((product) => {
-      if (product) {
-        res.locals.product = product;
-        return next();
-      }
-      next({ status: 404, message: `Product cannot be found.` });
-    })
-    .catch(next);
+async function productExists(req, res, next) {
+  const product = await productsService.read(req.params.productId);
+  if (product) {
+    res.locals.product = product;
+    return next();
+  }
+  next({ status: 404, message: `Product cannot be found.` });
 }
 
 module.exports = {
